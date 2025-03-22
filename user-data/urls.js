20b4427c7619b40db21f3266cfd10909f0740a9f
michaellui67 <michael.lui67@gmail.com>
2025-03-22 13:53:59 +0700

    Update urls.js

--- user-data/urls.js
+++ user-data/urls.js
@@ -2,7 +2,7 @@ const githubUsername = "michaellui67";
 const mediumUsername = "michael.lui67";
 
 const createMediumURL = (username) => `https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@${username}`;
-const gitRepos = (username) => `https://github.com/${username}.atom`;
+const gitRepos = (username) => `https://pinned.berrysauce.dev/get/${username}`;
 
 export const URLs = {
     medium: createMediumURL(mediumUsername),
